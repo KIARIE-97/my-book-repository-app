6bd3b4300ffd4065d8d7234a5a0ad46c60324c1c
sarah kiarie <sarahwanjiruki1@gmail.com>
2024-06-27 10:04:53 +0300

    correction

--- src/api.ts
+++ src/api.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://books-api-hyqf.onrender.com/', // replace with your backend URL
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // },
 });
 
 export default api;
